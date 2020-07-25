5bf338f63fb9f7bf1e94f7732debcd62b93c5c66
Juliana Ribeiro <jumaju10@hotmail.com>
2020-07-25 18:59:33 -0300

    GlobalStyles -add

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+import GlobalStyles from './styles/GlobalStyles';
+import Layout from './components/Layout';
+
 function App() {
   return (
-    <h1>Juu</h1>
+    <>
+      <Layout/>
+
+      <GlobalStyles/>
+    </>
   );
 }
 
