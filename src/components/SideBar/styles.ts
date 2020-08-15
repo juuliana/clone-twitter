6e14811cec2df998bb547c1a10668b1cdabb6761
Juliana Ribeiro <jumaju10@hotmail.com>
2020-08-15 14:26:40 -0300

    SideBar styles -add

---
+++ src/components/SideBar/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+import { Search } from '../../styles/Icons';
+
+export const Container = styled.div`
+    
+`;
+
+export const SearchWrapper = styled.div``;
+
+export const SearchInput = styled.div``;
+
+export const SearchIcon = styled(Search)`
+    width: 27px;
+    height: 27px;
+
+    fill: var(--gray);
+`;
+
+export const Body = styled.div``;
+
