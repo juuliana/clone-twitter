c3e66830570f47b44c1f1c9ec840d68e003b7f5f
Juliana Ribeiro <jumaju10@hotmail.com>
2020-08-15 14:25:49 -0300

    Layout updated

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import MenuBar from '../MenuBar';
 import Main from '../Main';
+import SideBar from '../SideBar';
 import { Container, Wrapper } from './styles';
 
 const Layout: React.FC = () => {
@@ -10,7 +11,7 @@ const Layout: React.FC = () => {
           <Wrapper>
               <MenuBar/>
               <Main/>
-              {/*<SideBar/>*/}
+              <SideBar/>
           </Wrapper>
       </Container>
   );
